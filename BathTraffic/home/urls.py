cb1800bead65493a68a5e4078fe624ebfd8f233e
u-jun <cat.1617@outlook.jp>
2023-09-06 17:41:50 +0900

    大体のフロントは完成した

--- BathTraffic/home/urls.py
+++ BathTraffic/home/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path,include
 from . import views
+#画像を読み込み
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('',views.home,name='home'),
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
