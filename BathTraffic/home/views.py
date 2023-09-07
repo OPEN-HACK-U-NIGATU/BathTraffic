a1b9d1c0b9fb2c7d1d2172f08365d40cf0676c30
Soma Ando <soma.ando.1227@gmail.com>
2023-09-07 17:00:24 +0900

    Test Edit

--- BathTraffic/home/views.py
+++ BathTraffic/home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 def home(request):
-    return render(request,"index.html")
+    with open('home/templates/index.html','r') as f:
+        html_content = f.read()
+    return HttpResponse(html_content,content_type='text/html')
