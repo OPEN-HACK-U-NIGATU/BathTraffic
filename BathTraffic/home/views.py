2ef452b0558e08f893ac46cdb934f01110fac80c
Soma Ando <soma.ando.1227@gmail.com>
2023-09-08 17:49:22 +0900

    Fixed conflict error

--- BathTraffic/home/views.py
+++ BathTraffic/home/views.py
@@ -1,19 +1,10 @@
-"""
-from django.shortcuts import render
-from django.http import HttpResponse
-
-def home(request):
-    # For testing front-end
-    # return render(request, "index.html") 
-    with open('home/templates/index.html','r') as f:
-        html_content = f.read()
-    return HttpResponse(html_content,content_type='text/html')
-"""
 from django.shortcuts import render
 from django.http import HttpResponse
 import os
 
 def home(request):
+    # For test Front-end
+    # return render(request, 'index.html')
     file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates/index.html')
     with open(file_path, 'r', encoding='utf-8') as f:
         html_content = f.read()
