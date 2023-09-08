1854e0bb3664693c1e19baecc11062f681744aaf
Soma Ando <soma.ando.1227@gmail.com>
2023-09-08 17:13:56 +0900

    Add Commentout to views.py

--- BathTraffic/home/views.py
+++ BathTraffic/home/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     # For testing front-end
-    # return render(request, "index.html")
+    # return render(request, "index.html") 
     with open('home/templates/index.html','r') as f:
         html_content = f.read()
     return HttpResponse(html_content,content_type='text/html')
