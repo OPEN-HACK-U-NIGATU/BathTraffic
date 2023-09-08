68e446969b9ba4fbc19772bf395b13f63bfebb99
Soma Ando <soma.ando.1227@gmail.com>
2023-09-08 18:40:53 +0900

    Complate

--- BathTraffic/home/views.py
+++ BathTraffic/home/views.py
@@ -3,8 +3,33 @@ from django.http import HttpResponse
 import os
 
 def home(request):
+    # 簡単な色変更用のプログラムを追加しました。
+    large = {"count": 7}
+    small = {"count": 2}
+    large_color = ""
+    small_color = ""
+
+    if large["count"] >= 0 and large["count"] < 4:
+        large_color = "green"
+    elif large["count"] >= 4 and large["count"] < 7:
+        large_color = "yellow"
+    else:
+        large_color = "red"
+    if small["count"] >= 0 and small["count"] < 3:
+        small_color = "green"
+    elif small["count"] >= 3 and small["count"] < 5:
+        small_color = "yellow"
+    else:
+        small_color = "red"
+        
+    large["color"] = large_color
+    small["color"] = small_color
+
     # For test Front-end
-    # return render(request, 'index.html')
+    # return render(request, 'index.html', {
+    #     "large": large,
+    #     "small": small
+    # })
     file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates/index.html')
     with open(file_path, 'r', encoding='utf-8') as f:
         html_content = f.read()
