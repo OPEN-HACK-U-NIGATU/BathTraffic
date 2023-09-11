f2fd8dd9ad99e6bd9ed1bbd2528dfbd546f05c95
hayato2475 <gto15723@gmail.com>
2023-09-11 18:00:23 +0900

    値の予測と計測データ(仮)の実装

---
+++ BathTraffic/Prophet.py
@@ -0,0 +1,29 @@
+import pandas as pd
+from prophet import Prophet
+import sqlite3
+
+conn = sqlite3.connect('BathTraffic\db.sqlite3')
+
+query = 'SELECT * FROM bath_number;'
+df = pd.read_sql_query(query,conn)
+
+# Prophetモデルの初期化
+model = Prophet()
+
+# データのカラム名をds（日付）とy（目的変数）に変更
+df.rename(columns={'ds': 'ds', 'big_number': 'y'}, inplace=True)
+
+# モデルにデータをフィット
+model.fit(df)
+
+# 予測を生成
+future = model.make_future_dataframe(periods=365)  # 予測期間を指定
+future.tail()
+
+forecast = model.predict(future)
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+
+fig1 = model.plot(forecast)
+
+fig1.show()
+input("Press Enter to close the plot...")
