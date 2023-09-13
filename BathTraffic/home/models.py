129d1b1fa4e2218285bf91c2d31bde30f01db3c3
ibu17 <yichuigaiqiao75@gmail.com>
2023-09-13 19:25:44 +0900

    model修正中

--- BathTraffic/home/models.py
+++ BathTraffic/home/models.py
@@ -4,3 +4,9 @@ class Snapshot(models.Model):
     small_number = models.PositiveIntegerField()
     big_number = models.PositiveIntegerField()
     time = models.DateTimeField(auto_now_add=True)
+
+class Prediction(models.Model):
+    p_small_number = 
+    p_big_number = 
+    time = 
+
